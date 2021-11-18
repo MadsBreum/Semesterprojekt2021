a83cefedbda53b11bcad37b8c7d9fe554d223b7a
k3Et <bniel18@student.sdu.dk>
2021-11-18 13:49:41 +0100

    Squashed commit of the following:
    commit 6dbcb6e30a8bce68955c152e951e5d412a30776e
    Author: k3Et <bniel18@student.sdu.dk>
    Date:   Thu Nov 18 13:48:57 2021 +0100
    
        Update LavaMap.unity
    
    commit 6d86a0c1d067f465187f56ffa7b85cbe4180c7d6
    Author: k3Et <bniel18@student.sdu.dk>
    Date:   Thu Nov 18 13:46:15 2021 +0100
    
        Update LavaMap.unity
    
    commit e0cab1a9e21a2ea2cfe07fb2916ec9cf92c546d3
    Author: k3Et <bniel18@student.sdu.dk>
    Date:   Thu Nov 18 13:45:42 2021 +0100
    
        Update LavaMap.unity
    
    commit 391eae54308a8fcb565f9f27926490978191e0c3
    Author: k3Et <bniel18@student.sdu.dk>
    Date:   Thu Nov 18 13:37:26 2021 +0100
    
        Finished LavaMap
    
    commit c9a29d2d1291b7dfbe28dfb8decd48a3ef253c7b
    Author: k3Et <bniel18@student.sdu.dk>
    Date:   Thu Nov 18 13:22:11 2021 +0100
    
        Fixed Maps
    
    commit 35b8d8969cc6f0c95f929da76b7a0f6fc4b4d824
    Author: k3Et <bniel18@student.sdu.dk>
    Date:   Thu Nov 18 13:01:53 2021 +0100
    
        Fixed Player 2 and added LavaMap/IceMap
    
    commit 8b3fa64664972fba5529ad20d2fb9a1af4cb001a
    Merge: 2b9296e a6d911b
    Author: k3Et <bniel18@student.sdu.dk>
    Date:   Thu Nov 18 12:06:19 2021 +0100
    
        Merge branch 'main' into MapSetup
    
    commit 2b9296e8b986809d55db93c6c9995056ca0aef40
    Author: k3Et <bniel18@student.sdu.dk>
    Date:   Thu Nov 18 11:56:31 2021 +0100
    
        MapSetup
    
        Starting with borders
    
    commit 9fcc81d6c8466ee145321649f8aeaafa2ebd8428
    Merge: c097824 5ec7184
    Author: k3Et <bniel18@student.sdu.dk>
    Date:   Thu Nov 18 11:20:15 2021 +0100
    
        Merge branch 'main' into MapSetup
    
    commit c097824735e5566c9bacd86448900bc46b1104d8
    Author: k3Et <bniel18@student.sdu.dk>
    Date:   Thu Nov 18 11:19:01 2021 +0100
    
        Start of map

--- Semesterprojekt 2021/Assets/Scripts/Player2MovementController.cs
+++ Semesterprojekt 2021/Assets/Scripts/Player2MovementController.cs
@@ -6,34 +6,54 @@ public class Player2MovementController : MonoBehaviour
 {
     // Start is called before the first frame update
     public Rigidbody2D Rb = new Rigidbody2D();
-    public BoxCollider2D Cc = new BoxCollider2D();
+    public BoxCollider2D Bc = new BoxCollider2D();
     public bool touchingGround;
-    float maxVel = 5;
+    public float maxVel = 5;
+    public float jump = 210;
+    public float speed = 10;
+    bool movingRight = true;
+
     void Start()
     {
         Rb.GetComponent<Rigidbody2D>();
-        Cc.GetComponent<BoxCollider2D>();
+        Bc.GetComponent<BoxCollider2D>();
         Rb.freezeRotation = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (movingRight == false)
+            {
+                Flip();
+            }
+            movingRight = true;
+        }
 
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (movingRight == true)
+            {
+                Flip();
+            }
+            movingRight = false;
+        }
     }
     void FixedUpdate()
     {
         if (Input.GetKey(KeyCode.W) && touchingGround)
         {
-            Rb.AddForce(transform.up * 210f);
+            Rb.AddForce(transform.up * jump);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            Rb.AddForce(transform.right * -10f);
+            Rb.AddForce(transform.right * speed);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            Rb.AddForce(transform.right * 10f);
+            Rb.AddForce(transform.right * speed);
         }
         if (Rb.velocity.x > maxVel)
         {
@@ -48,7 +68,7 @@ public class Player2MovementController : MonoBehaviour
 
     void OnCollisionExit2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Platform" && collision.otherCollider == Cc)
+        if (collision.gameObject.tag == "Platform" && collision.otherCollider == Bc)
         {
             touchingGround = false;
         }
@@ -56,8 +76,12 @@ public class Player2MovementController : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Platform" && collision.otherCollider == Cc)
+        if (collision.gameObject.tag == "Platform" && collision.otherCollider == Bc)
             touchingGround = true;
     }
 
+    private void Flip()
+    {
+        transform.Rotate(new Vector3(0f, 180f, 0f));
+    }
 }
